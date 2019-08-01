24b2c369158d6847f5b915435a4160c402a514f4
Jonathan Forney <jonathan.forney@gmail.com>
2019-07-31 17:50:26 -0700

    front end SUCCESS

--- src/signIn.js
+++ src/signIn.js
@@ -1,18 +1,18 @@
 const form = document.getElementById('user-form');
-const p = document.getElementById('sign-in-message')
+const p = document.getElementById('message');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const userName = formData.get('userName');
+  const username = formData.get('userName');
   const password = formData.get('password');
 
-  return fetch('https://joliliah.herokuapp.com/api/v1/auth/signin', {
+  return fetch('http://localhost:7890/api/v1/auth/signin', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ userName, password }),
+    body: JSON.stringify({ username, password }),
     credentials: 'include'
   })
     .then(res => {
