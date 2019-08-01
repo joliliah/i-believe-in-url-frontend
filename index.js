ea0fe06f6f2bd6742883ec189532935d63b32b5b
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-01 11:43:59 -0700

    route for posting url

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 import checkAuth from './utils/check-auth.js';
 
 checkAuth();
+
+const form = document.getElementById('url-form');
+const p = document.getElementById('short-url');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const longURL = formData.get('long-url');
+
+  return fetch('https://joliliah.herokuapp.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ originalURL: longURL }),
+    credentials: 'include'
+  })
+    .then(res => {
+      console.log('sign in', res);
+      if(res.ok === true) {
+        console.log('we are hitting our if block in signin js')
+        p.textContent = `https://joliliah.herokuapp.com/${res.shortURLId}`
+      } 
+      else {
+        console.log('shorty aint working')
+        p.textContent = 'NO BUENO FAM'; 
+      }
+    })
+})
